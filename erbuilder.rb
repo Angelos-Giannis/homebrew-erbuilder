cc0c9e8e8f42d61eb7864fe17abb95e56742196a
Angelos Giannis <38913957+Angelos-Giannis@users.noreply.github.com>
2020-07-19 02:45:48 +0200

    Simplify homebrew download

--- erbuilder.rb
+++ erbuilder.rb
@@ -1,37 +1,16 @@
-HOMEBREW_ERBUILDER_VERSION="0.3.3"
 class Erbuilder < Formula
   desc "CLI tool to generate an .er file based on profided go structures."
   homepage "https://github.com/Angelos-Giannis/erbuilder"
-  url "https://github.com/Angelos-Giannis/erbuilder/releases/download/v#{HOMEBREW_ERBUILDER_VERSION}/erbuilder-v#{HOMEBREW_ERBUILDER_VERSION}-darwin-amd64.tar.gz"
-
-  version HOMEBREW_ERBUILDER_VERSION
+  url "https://github.com/Angelos-Giannis/erbuilder/releases/download/v0.3.3/erbuilder-v0.3.3-darwin-amd64.tar.gz"
+  version "v0.3.3"
   sha256 "4bb5b6d5d4d7e7da1cfc2d48bce9945448bdfdc6caeaeda1ccbf7fa618c64b13"
 
-  head "https://github.com/Angelos-Giannis/erbuilder.git", :tag => "#{HOMEBREW_ERBUILDER_VERSION}"
-
-  if build.head?
-    depends_on "go" => :build
+  if OS.linux?
+    url "https://github.com/Angelos-Giannis/erbuilder/releases/download/v0.3.3/erbuilder-v0.3.3-linux-amd64.tar.gz"
+    sha256 "d8416a6a7f6fd83bad77221ef8f89416c404b7ce3674fc22b8e5e41c93a334e7"
   end
 
   def install
-    if build.head?
-      ENV["GOPATH"] = buildpath
-      ENV.prepend_create_path "PATH", buildpath/"bin"
-      dir = buildpath/"src/github.com/Angelos-Giannis/erbuilder"
-      dir.install buildpath.children - [buildpath/".brew_home"]
-
-      cd dir do
-        system "go", "build . -o erbuilder"
-        cp "erbuilder", buildpath
-      end
-    else
-      mv "erbuilder-v#{HOMEBREW_ERBUILDER_VERSION}-darwin-amd64", "erbuilder"
-    end
     bin.install "erbuilder"
-    prefix.install_metafiles
-  end
-
-  test do
-    assert_match 'erbuilder', shell_output("#{bin}/erbuilder --help", 1)
   end
 end
