f24f1378ad434382bda22ace4e1ac1937a6821c3
Angelos Giannis <angelos.giannis@gmail.com>
2020-07-20 01:26:47 +0200

    Update version to v0.3.4

--- erbuilder.rb
+++ erbuilder.rb
@@ -1,13 +1,13 @@
 class Erbuilder < Formula
   desc "CLI tool to generate an .er file based on profided go structures."
   homepage "https://github.com/eujoy/erbuilder"
-  url "https://github.com/eujoy/erbuilder/releases/download/v0.3.3/erbuilder-v0.3.3-darwin-amd64.tar.gz"
-  version "v0.3.3"
-  sha256 "5cc5e2b1c7f03e8759206e1dd36abe90bc4ef912f43d9e140ec5d26115c44888"
+  url "https://github.com/eujoy/erbuilder/releases/download/v0.3.4/erbuilder-v0.3.4-darwin-amd64.tar.gz"
+  version "v0.3.4"
+  sha256 "44c1cf30c7717e9f9cfcdef7e0e459ec1dc4e456df8b9f6fa911f8ab6fa11e42"
 
   if OS.linux?
-    url "https://github.com/eujoy/erbuilder/releases/download/v0.3.3/erbuilder-v0.3.3-linux-amd64.tar.gz"
-    sha256 "6621e07eca2c22233209b98f97b6da3b10e22ec7b0380bf4303654f32d3b587b"
+    url "https://github.com/eujoy/erbuilder/releases/download/v0.3.4/erbuilder-v0.3.4-linux-amd64.tar.gz"
+    sha256 "809385c85d8a9a4eda8e2eee9e583d0d71261ce1e3ec574acf6142d048b1767b"
   end
 
   def install
