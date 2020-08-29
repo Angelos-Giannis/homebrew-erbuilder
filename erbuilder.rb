0706ecbade77ca9fe3c58430960e6b48e7550a50
Angelos Giannis <angelos.giannis@gmail.com>
2020-08-29 23:47:31 +0200

    Update sha values

--- erbuilder.rb
+++ erbuilder.rb
@@ -3,11 +3,11 @@ class Erbuilder < Formula
   homepage "https://github.com/eujoy/erbuilder"
   url "https://github.com/eujoy/erbuilder/releases/download/v0.6.0/erbuilder-v0.6.0-darwin-amd64.tar.gz"
   version "v0.6.0"
-  sha256 "e118daa401633cea78d052be38740c92e11eb030244f49e18fb243c337635220"
+  sha256 "aa6ca108012d4b5fb9d54ca60713056c8212d84a88d6a8f7777977a7500bd985"
 
   if OS.linux?
     url "https://github.com/eujoy/erbuilder/releases/download/v0.6.0/erbuilder-v0.6.0-linux-amd64.tar.gz"
-    sha256 "6fc906c2cf9290b0841034d35f26dcdf13ae2d39a28b0ec911c49c78611469e8"
+    sha256 "cbac399cb0e0215bdb0e6960d15837d7ded6f1b2e0e33c3037476bcb34f32704"
   end
 
   def install
